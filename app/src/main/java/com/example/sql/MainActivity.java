04c7b1246611c99d57743c84ce01e504aeba3b6c
dz2244 <dz2244@bs.amalnet.k12.il>
2025-05-01 20:07:47 +0300

    AAA

--- app/src/main/java/com/example/sql/MainActivity.java
+++ app/src/main/java/com/example/sql/MainActivity.java
@@ -1,10 +1,20 @@
 package com.example.sql;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
+    SQLiteDatabase db ;
+    HelperDB hlp ;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        hlp = new HelperDB(this);
+        db = hlp.getWritableDatabase();
+        db.close();
     }
 }
