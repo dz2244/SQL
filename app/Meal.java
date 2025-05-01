f1b237cdf915c6f4832cc2a7926ca3b8d6eea3ba
dz2244 <dz2244@bs.amalnet.k12.il>
2025-05-01 19:52:18 +0300

    AAA

--- app/Meal.java
+++
@@ -1,8 +0,0 @@
-public class Meal {
-    public static final String KEY_ID = "_id";
-    public static final String TABLE_MEAL= "Worker";
-    public static final String FIRST_MEAL = "FirstMeal";
-    public static final String MAIN_MEAL = "MainMeal";
-    public static final String SIDE_MEAL = "SideMeal";
-    public static final String DESSERT = "Dessert";
-}
--- app/ParkFood.java
+++
@@ -1,8 +0,0 @@
-public class ParkFood {
-    public static final String KEY_ID = "_id";
-    public static final String TABLE_PARKFOOD= "Worker";
-    public static final String COMPANY_ID= "CompanyID";
-    public static final String COMPANY_NAME = "CompanyName";
-    public static final String MAIN_PHONE = "MainPhone";
-    public static final String SECONDARY_PHONE = "SecondaryPhone";
-}
