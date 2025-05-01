c03a3f4abba8c4e0b01c9f90ccccea195f916dd8
dz2244 <dz2244@bs.amalnet.k12.il>
2025-05-01 19:46:46 +0300

    AAA

---
+++ app/src/main/java/com/example/sql/WorkerA.java
@@ -0,0 +1,10 @@
+package com.example.sql;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+public class WorkerA extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_worker);
+    }
+}
